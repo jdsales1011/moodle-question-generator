10406f2027d185e779129b074d2ed9bd92057d33
jdsales1011 <jydeesales@gmail.com>
2023-03-09 03:20:45 +0800

    Working OpenAI code locally.

--- python_logic.py
+++ python_logic.py
@@ -1,20 +1,19 @@
 import sys
 import os
 
-# !pip install --upgrade openai
-# import openai
+import openai
 
-# os.environ['OPENAI_API_KEY']='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'
-# openai.api_key = os.getenv("OPENAI_API_KEY")
+os.environ['OPENAI_API_KEY']='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'
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-context = sys.argv[1]
-number = int (sys.argv[2])
-q_type = int(sys.argv[3])
-# number = 2
-# q_type = 1
-# context = '''
-# Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary.
-# '''
+# context = sys.argv[1]
+# number = int (sys.argv[2])
+# q_type = int(sys.argv[3])
+number = 2
+q_type = 1
+context = '''
+Architecturally, the school has a Catholic character. Atop the Main Building's gold dome is a golden statue of the Virgin Mary.
+'''
 
 
 def prompt_creator1(context, num=1): #identification
@@ -38,39 +37,38 @@ prompt_creators = {
 
 
 # AI PREDICT FUNCTION
-# def predict_questions(prompt):
-#   result =[]
-#   openai.api_key = os.getenv("OPENAI_API_KEY")
+def predict_questions(prompt):
+  result =[]
+  openai.api_key = os.getenv("OPENAI_API_KEY")
 
-#   response = openai.Completion.create(
-#     model="text-davinci-003",
-#     prompt=prompt,
-#     temperature=0.7,
-#     max_tokens=256,
-#     top_p=1,
-#     frequency_penalty=0,
-#     presence_penalty=0
-#   )
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
 
-#   new = response['choices'][0]['text']
-# #   result = new.split('\n')
+  new = response['choices'][0]['text']
+#   result = new.split('\n')
   
 
-# #   result = [x for x in result if x.strip()]       # TO REMOVE EMPTY NEXT LINES
+#   result = [x for x in result if x.strip()]       # TO REMOVE EMPTY NEXT LINES
 
-#   # for index, data in enumerate(result):
-#   #   if index%2 == 0:          # QUESTION
-#   #     result[index] = data.split(".", 1)[1].strip()
-#   #   else:                     # ANSWER
-#   #     result[index] = data.split("Answer:", 1)[1].strip()
+  # for index, data in enumerate(result):
+  #   if index%2 == 0:          # QUESTION
+  #     result[index] = data.split(".", 1)[1].strip()
+  #   else:                     # ANSWER
+  #     result[index] = data.split("Answer:", 1)[1].strip()
 
-# #   return result
-#   return new
+#   return result
+  return new
 
 
 prompt = prompt_creators[q_type]
 print(prompt)
-print("testing")
 
 # PREDICT CODE HERE
-# print(predict_questions(prompt))
+print(predict_questions(prompt))
