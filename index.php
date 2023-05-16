8de4e310906a52a581186819df5a2b0a02629cea
jdsales1011 <jydeesales@gmail.com>
2023-05-17 04:57:29 +0800

    Initial edit of index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - https://moodle.org/
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,115 +12,101 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * List all files in a course.
+ *
  * @package     local_questiongenerator
  * @copyright   2023 Jivielyn Sales <jivielyn.sales@gmail.com>
  * @copyright   based on work by 2017 Martin Gauk (@innoCampus, TU Berlin) and 2022 Kacper Rokicki <k.k.rokicki@gmail.com>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-
- // INITIAL CODE.
-require_once('../../config.php');
-require_once($CFG->dirroot. '/local/questiongenerator/lib.php');
-require_once($CFG->dirroot. '/local/questiongenerator/generation_form.php');
-
-$context = context_system::instance();
-$PAGE->set_context($context);
-$PAGE->set_url(new moodle_url('/local/questiongenerator/index.php'));
-$PAGE->set_pagelayout('incourse');
-$PAGE->set_title($SITE->fullname);
-$PAGE->set_heading(get_string('pluginname', 'local_questiongenerator'));
-
-// API.
-$ch = curl_init();          // Initialize a new cURL session.
-$url = "http://127.0.0.1:2000/qgplugin/api/";
-curl_setopt($ch, CURLOPT_URL, $url);
-// Set the request headers.
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: multipart/form-data'
-));
+use core\notification;
 
 
-// INITIALIZE FORM.
-$generatorform = new local_questiongenerator_generation_form();
+require_once(dirname(__FILE__) . '/../../config.php');
 
-// DISPLAYING OF PAGE.
-echo $OUTPUT->header();
+// Import the question_bank class
+require_once($CFG->dirroot . '/question/engine/bank.php');
+require_once($CFG->dirroot . '/question/engine/lib.php');
+require_once($CFG->libdir . '/questionlib.php');
+require_once($CFG->dirroot . '/question/type/questiontypebase.php');
 
-echo '<h4> Generate questions based on the inputed article/resource </h4>';
+global $DB, $PAGE;
 
-$generatorform->display();
+$courseid = required_param('courseid', PARAM_INT);
+$component = optional_param('component', 'all', PARAM_ALPHANUMEXT);
+$filetype = optional_param('filetype', 'all', PARAM_ALPHAEXT);
+$action = optional_param('action', '', PARAM_ALPHAEXT);
+$chosenfiles = optional_param_array('file', array(), PARAM_INT);
+$num_questions = optional_param('num_ques', 1, PARAM_INT);
+$type_questions = optional_param('type_question', 1, PARAM_INT);
+$chosen_questions = optional_param_array('question', array(), PARAM_INT);
+$file_name = optional_param('file_name', '', PARAM_ALPHANUMEXT);
 
-// If form is submitted, retrieve data from form.
-if ($data = $generatorform->get_data()) {           // add optional AND $attachment_file
-    $number = required_param('num_ques', PARAM_INT);
-    $qtype = required_param('type_question', PARAM_INT);
-
-    $content = "";
-
-    if ($name = $generatorform->get_new_filename('uploaded_file')) {
-        // Save text file content to content variable.
-        // $content = $generatorform->get_file_content('uploaded_file');
-
-        $file_name = $generatorform->get_new_filename('uploaded_file');
-        $file_path = "temp/".$file_name;
-        $success = $generatorform->save_file('uploaded_file', $file_path);
-
-        if(!$success){
-            echo "Oops! Something went wrong!";
-            $content = required_param('content', PARAM_TEXT);
-        }
-        else{
-            // Create file handle.
-            $file_handle = fopen($file_path, 'r');
-
-            // SET THE CONTENT AS THE FILE HERE
-            echo "File saved successfully in {$file_path} <br>";
-        }
-    }
-    else {
-        // Get the content from the text field.
-        $content = required_param('content', PARAM_TEXT);
-        // echo "no attachment found";
-    }
-
-    // POST REQUEST.
-    $data_array = array(
-        'file' => curl_file_create($file_path),
-        'content' => $content,
-        'number' => $number,
-        'type' => $qtype
-    );
-
-    $data = json_encode($data_array);
-    // "DAtaa: {$data_array} <br> {$data} <br>";
-
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_array);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-
-    $response = curl_exec($ch);
-
-    echo $response;
-
-    if($err = curl_error($ch)) {
-        echo $err;
-    }
-    else {
-        $decoded = json_decode($response, true);
-        print_r($decoded);
-
-        // foreach($decoded as $key => $val) {
-        //     echo "{$key}:" , implode("<br>", $val), "<br><br>";
-        // }
-    }
-
-    fclose($file_handle);
-    curl_close($ch);
+if (!$course = $DB->get_record('course', array('id' => $courseid))) {
+    throw new moodle_exception('invalidcourseid');
 }
 
-echo $OUTPUT->footer();
+$context = context_course::instance($courseid);
+$title = get_string('pluginname', 'local_questiongenerator');
+
+$params = array('courseid' => $courseid);
+$componentparams = array('courseid' => $courseid);
+$filetypeparams = array('courseid' => $courseid);
+if ($component != 'all') {
+    $params['component'] = $component;
+    $filetypeparams['component'] = $component;
+}
+if ($filetype != 'all') {
+    $params['filetype'] = $filetype;
+    $componentparams['filetype'] = $filetype;
+}
+
+$url = new moodle_url('/local/questiongenerator/index.php', $params);
+$componenturl = new moodle_url('/local/questiongenerator/index.php', $componentparams);
+$filetypeurl = new moodle_url('/local/questiongenerator/index.php', $filetypeparams);
+
+$PAGE->set_url($url);
+$PAGE->set_context($context);
+$PAGE->set_title($title);
+$PAGE->set_heading($course->fullname);
+$PAGE->set_pagelayout('incourse');
+
+require_login($courseid);
+require_capability('local/questiongenerator:view', $context);
+$downloadallowed = has_capability('local/questiongenerator:download', $context);
+
+$coursefiles = new local_questiongenerator\course_files($courseid, $context, $component, $filetype);
+
+$renderer = $PAGE->get_renderer('local_questiongenerator');
+
+
+if ($action === 'generate') {
+    require_sesskey();
+    try {
+        $questions_dir = $coursefiles->generate_questions($chosenfiles, $num_questions, $type_questions);
+        
+        echo $OUTPUT->header();
+        echo $renderer->question_selection_page($type_questions, $questions_dir);
+        echo $OUTPUT->footer();
+    } catch (moodle_exception $e) {
+        notification::add($e->getMessage(), \core\output\notification::NOTIFY_ERROR);
+    }
+}
+else if ($action === 'save'){
+    echo $OUTPUT->header();
+    $temp_dir = ".temp/";
+    $file_path = $temp_dir . $file_name . ".txt";
+    $params = $coursefiles->save_questions($url, $course, $courseid, $context, $chosen_questions, $file_path, $type_questions);
+    echo $OUTPUT->continue_button(new moodle_url('/question/edit.php', $params));
+    echo $OUTPUT->footer();
+    
+}
+else{
+    echo $OUTPUT->header();
+    echo $renderer->overview_page($url, $componenturl, $filetypeurl, $coursefiles, $downloadallowed);
+    echo $OUTPUT->footer();
+}
